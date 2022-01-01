5ca60085cd2527fe7a6daa95781beb38ddfed2e6
Jake Coffman <jake@jakecoffman.com>
2022-01-01 17:53:34 -0600

    build for github

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'dist',
+			assets: 'dist',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: dev ? '' : '/sudoku',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
